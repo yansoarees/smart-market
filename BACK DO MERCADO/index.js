12c5057220d362877a61a6f861639a2ce8cee794
Yan <yan.soarees@outlook.com>
2026-05-29 17:49:52 -0300

    feat: upload de imagens, rotas de login e novos status de pedido

--- BACK DO MERCADO/index.js
+++ BACK DO MERCADO/index.js
@@ -11,7 +11,14 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+    origin: [
+        "https://smart-market-beta-ten.vercel.app", 
+        "http://localhost:5173"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 app.use('/uploads', express.static('uploads')); // Permite ver as fotos salvas
 
