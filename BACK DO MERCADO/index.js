c3ad0424a8d7f18424fdad7e0a03d5b2c1ef0722
Yan <yan.soarees@outlook.com>
2026-06-02 09:09:54 -0300

    Muda a porta para 3000 para alinhar com o proxy do Railway

--- BACK DO MERCADO/index.js
+++ BACK DO MERCADO/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 // Pegando a porta dinâmica do Railway obrigatoriamente
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send("🔥 SERVIDOR VIVO E RESPIRANDO NO RAILWAY! A culpa é do banco de dados!");
